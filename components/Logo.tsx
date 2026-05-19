5c92de76051361cf5e37bd90932e9635b2b9123d
Cleve-keem <belloharyourmidey@gmail.com>
2026-05-19 16:26:06 -0500

    fix(auth): added priority and size to logo image

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="size-15 relative">
-      <Image src="/logo2.png" alt="logo" fill className="object-cover" />
+      <Image
+        src="/logo2.png"
+        alt="logo"
+        fill
+        className="object-cover"
+        priority
+        sizes=""
+      />
     </div>
   );
 }
