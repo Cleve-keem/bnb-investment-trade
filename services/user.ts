489a012c0383563df57710b4e335f6886ff8bfd5
cleve-keem <belloharyourmidey@gmail.com>
2026-07-30 14:59:11 +0100

    fix send reset password error link not a module

--- services/user.ts
+++ services/user.ts
@@ -1,24 +1,26 @@
-// import supabase from "@/libs/supabase/browser";
+import { createClient } from "@/libs/supabase/browser";
 
-// export default class UserService {
-//   static async getUserByEmail(email: string) {
-//     const { data: userProfile, error: profileError } = await supabase
-//       .from("users")
-//       .select("id, first_name")
-//       .eq("email", email)
-//       .maybeSingle();
+export default class UserService {
+  static async getUserByEmail(email: string) {
+    const supabase = createClient();
+    const { data: userProfile, error: profileError } = await supabase
+      .from("users")
+      .select("id, first_name")
+      .eq("email", email)
+      .maybeSingle();
 
-//     return { userProfile, profileError };
-//   }
+    return { userProfile, profileError };
+  }
 
-//   static async getUserProfile(userId: string) {
-//     const { data, error } = await supabase
-//       .from("users")
-//       .select("*")
-//       .eq("id", userId)
-//       .single();
+  static async getUserProfile(userId: string) {
+    const supabase = createClient();
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", userId)
+      .single();
 
-//     if (error) throw error;
-//     return data;
-//   }
-// }
+    if (error) throw error;
+    return data;
+  }
+}
